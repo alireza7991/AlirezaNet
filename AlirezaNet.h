71e0a3919d50bec50b1f39a52269be5dd7b540d9
alireza7991 <alirezafn@gmx.us>
2017-04-03 04:09:07 +0430

    add MLP

---
+++ AlirezaNet.h
@@ -0,0 +1,116 @@
+//
+// Created by alirezafn on 4/2/17.
+//
+
+#ifndef ALIREZANET_ALIREZANET_H
+#define ALIREZANET_ALIREZANET_H
+
+#include <Eigen/Dense>
+#include <Eigen/Eigen>
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+namespace Alireza {
+
+    using namespace Eigen;
+    typedef Matrix<float, Dynamic, Dynamic> tensor;
+
+    using std::vector;
+
+    tensor _sig(tensor &input) {
+        tensor tmp = input;
+        for (unsigned i = 0; i < tmp.rows(); i++) {
+            for (unsigned j = 0; j < tmp.cols(); j++) {
+                tmp(i, j) = (1 / (1 + std::exp(-tmp(i, j))));
+            }
+        }
+        return tmp;
+    }
+
+    tensor _sig_d(tensor &input) {
+        tensor tmp = input;
+        for (unsigned i = 0; i < tmp.rows(); i++) {
+            for (unsigned j = 0; j < tmp.cols(); j++) {
+                tmp(i, j) = tmp(i, j) * (1 - tmp(i, j));
+            }
+        }
+        return tmp;
+    }
+
+    class NueralNet {
+    public:
+        NueralNet(float momentum, float learningRate, unsigned inputSize, unsigned outputSize, unsigned hiddenSize) {
+            this->momentum = momentum;
+            this->learningRate = learningRate;
+            this->hiddenSize = hiddenSize;
+            this->outputSize = outputSize;
+            this->inputSize = inputSize;
+            l1Weights.resize(hiddenSize + 1, inputSize + 1);
+            w1Update.resize(hiddenSize + 1, inputSize + 1);
+            input.resize(inputSize + 1, 1);
+            w2Update.resize(outputSize, hiddenSize + 1);
+            l2Weights.resize(outputSize, hiddenSize + 1);
+            w1Update.setZero();
+            w2Update.setZero();
+            l2Weights.setRandom();
+            l2Weights.setRandom();
+        }
+
+        void train(vector<tensor &> sample, vector<tensor &> groundTruth, unsigned epoch) {
+            assert(input.size() == groundTruth.size());
+            for (int i = 0; i < epoch; i++) {
+                for (int j = 0; j < sample.size(); j++) {
+                    std::cout << "training sample ..." << std::endl;
+                    train_one(sample[j], groundTruth[j]);
+                }
+            }
+        }
+
+        void train_one(tensor &sample, tensor &groundTruth) {
+            forward(sample);
+            backward(groundTruth);
+        }
+
+        tensor test(tensor &sample) {
+            forward(sample);
+            return output;
+        }
+
+    private:
+        float momentum;
+        float learningRate;
+        unsigned inputSize, outputSize, hiddenSize;
+        tensor l1Weights, l2Weights;
+        tensor w1Update, w2Update;
+        tensor input, output, l1Out;
+        tensor l1Error, l2Error;
+
+        tensor forward(tensor &sample) {
+            for (long i = 0; i < sample.rows(); ++i)
+                input(i) = sample(i);
+            input(input.rows() - 1) = -1;
+            l1Out = _sig(l1Weights * input);
+            l1Out(l1Out.rows() - 1) = -1;
+            output = l2Weights * l1Out;
+        }
+
+        void backward(tensor &groundTruth) {
+            l2Error = Eigen::MatrixXf::Ones(outputSize, 1) - output;
+            l2Error.cwiseProduct(output);
+            l2Error.cwiseProduct(groundTruth - output);
+            l1Error = Eigen::MatrixXf::Ones(hiddenSize + 1, 1) - l1Out;
+            l1Error.cwiseProduct(l1Out);
+            l1Error.cwiseProduct(l2Weights.transpose() * l2Error);
+            w2Update = this->learningRate * l2Error * l1Out.transpose() + momentum * w2Update;
+            w1Update = this->learningRate * l1Error * input.transpose() + momentum * w1Update;
+            l1Weights += w1Update;
+            l2Weights += w2Update;
+        }
+
+    };
+
+
+}
+
+#endif //ALIREZANET_ALIREZANET_H
--- test.cpp
+++ test.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include "AlirezaNet.h"
 
+using namespace Alireza;
 using namespace std;
 using namespace Eigen;
 
 int main() {
+    NueralNet net(0.8, 0.01, 2, 1, 10);
+    tensor x1(2, 1), x2(2, 1), x3(2, 1), x4(2, 1);
+    tensor y1(2, 1), y2(2, 1), y3(2, 1), y4(2, 1);
+    x1(0, 0) = 0;
+    x1(1, 0) = 0;
+    y1(0, 0) = 0;
+    x2(0, 0) = 1;
+    x2(1, 0) = 0;
+    y2(0, 0) = 1;
+    x3(0, 0) = 0;
+    x3(1, 0) = 1;
+    y3(0, 0) = 1;
+    x4(0, 0) = 1;
+    x4(1, 0) = 1;
+    y4(0, 0) = 0;
+    vector<tensor &> sample, groundTruth;
+    sample.push_back(x1);
+    groundTruth.push_back(y1);
+    sample.push_back(x2);
+    groundTruth.push_back(y2);
+    sample.push_back(x3);
+    groundTruth.push_back(y3);
+    sample.push_back(x3);
+    groundTruth.push_back(y3);
+    // train!
+    cout << "Training ... " << endl;
+    net.train(sample, groundTruth, 100);
+    cout << "Testing ... " << endl;
+    tensor e = net.test(x1);
+    cout << "0 , 0 => " << e(0, 0) << endl;
+    e = net.test(x2);
+    cout << "1 , 0 => " << e(0, 0) << endl;
+    e = net.test(x3);
+    cout << "0 , 1 => " << e(0, 0) << endl;
+    e = net.test(x4);
+    cout << "1 , 1 => " << e(0, 0) << endl;
+
+
 }
